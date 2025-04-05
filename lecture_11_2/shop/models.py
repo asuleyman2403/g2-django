de041bc3327d494db001f573d5ad9f16289f7df3
Assyl Suleiman <asuleyman2403@gmail.com>
2025-04-05 12:51:18 +0500

    fix: chagnes

--- lecture_11_2/shop/models.py
+++ lecture_11_2/shop/models.py
@@ -13,6 +13,8 @@ class Category(models.Model):
         return f'ID: {self.id} {self.name}'
 
 
+print('changed something asdfasd asdf')
+
 class Product(models.Model):
     name = models.CharField(null=False, max_length=255, blank=False)
     description = models.CharField(null=False, max_length=2000, blank=True, default='')
