7f61019160327bb6ab7f34ef7f7cf0e9e52caeac
Assyl Suleiman <asuleyman2403@gmail.com>
2025-04-05 13:47:34 +0500

    fix: set debug false

--- lecture_11_2/lecture_3/settings.py
+++ lecture_11_2/lecture_3/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z3nt&$dywcy1$y-xs4_)l-o%qunk=e5ar!=6r@7s5c968c3b7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'asuleiman.pythonanywhere.com'
